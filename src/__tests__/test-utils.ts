d7de72a26df16e00b96e40ced411f3b8a37dd52b
Matthew Scharley <matt@scharley.me>
2024-04-09 04:33:04 +1000

    chore: lint

--- src/__tests__/test-utils.ts
+++ src/__tests__/test-utils.ts
@@ -5,16 +5,16 @@ import type { ChangesetsConfiguration } from '../model/ChangesetsConfiguration';
 export declare type DeepPartial<T> =
 	| T
 	| (T extends Array<infer U>
-			? Array<DeepPartial<U>>
-			: T extends Map<infer K, infer V>
-				? Map<DeepPartial<K>, DeepPartial<V>>
-				: T extends Set<infer M>
-					? Set<DeepPartial<M>>
-					: T extends object
-						? {
-								[K in keyof T]?: DeepPartial<T[K]>;
-							}
-						: T);
+		? Array<DeepPartial<U>>
+		: T extends Map<infer K, infer V>
+			? Map<DeepPartial<K>, DeepPartial<V>>
+			: T extends Set<infer M>
+				? Set<DeepPartial<M>>
+				: T extends object
+					? {
+							[K in keyof T]?: DeepPartial<T[K]>;
+						}
+					: T);
 
 export const changesets = (inp: DeepPartial<ChangesetsConfiguration>): ChangesetsConfiguration =>
 	inp as ChangesetsConfiguration;
--- src/io/github/getFile.ts
+++ src/io/github/getFile.ts
@@ -3,26 +3,26 @@ import { debug } from '@actions/core';
 import type { getOctokit } from '@actions/github';
 import type { TypeGuard } from 'generic-type-guard';
 
-export const getFile =
-	(octokit: ReturnType<typeof getOctokit>, owner: string, repo: string, ref: string) =>
+export const getFile
+	= (octokit: ReturnType<typeof getOctokit>, owner: string, repo: string, ref: string) =>
 	<T>(guard: TypeGuard<T>) =>
-	async (path: string): Promise<[string, T]> => {
-		debug(`Fetching package from ${owner}/${repo}/${ref}:${path}`);
-		const packageJsonResponse = await octokit.rest.repos.getContent({
-			owner,
-			repo,
-			ref,
-			path,
-			mediaType: {
-				format: 'raw',
-			},
-		});
+			async (path: string): Promise<[string, T]> => {
+				debug(`Fetching package from ${owner}/${repo}/${ref}:${path}`);
+				const packageJsonResponse = await octokit.rest.repos.getContent({
+					owner,
+					repo,
+					ref,
+					path,
+					mediaType: {
+						format: 'raw',
+					},
+				});
 
-		if (typeof packageJsonResponse.data !== 'string') {
-			throw new Error(`Invalid data when retrieving package file: ${owner}/${repo}/${ref}:${path}`);
-		}
-		const packageJson: unknown = JSON.parse(packageJsonResponse.data);
-		assert(packageJson, guard, `Invalid contents for file ${owner}/${repo}/${path}`);
+				if (typeof packageJsonResponse.data !== 'string') {
+					throw new Error(`Invalid data when retrieving package file: ${owner}/${repo}/${ref}:${path}`);
+				}
+				const packageJson: unknown = JSON.parse(packageJsonResponse.data);
+				assert(packageJson, guard, `Invalid contents for file ${owner}/${repo}/${path}`);
 
-		return [path, packageJson];
-	};
+				return [path, packageJson];
+			};
