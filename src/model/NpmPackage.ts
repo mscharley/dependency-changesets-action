fda3b12b3caac1440f70123401a4efbdb3705c91
Brad Adams <hi@breadadams.com>
2025-06-20 11:42:30 +0200

    pr feedback + build
    Signed-off-by: Brad Adams <hi@breadadams.com>

--- src/model/NpmPackage.ts
+++ src/model/NpmPackage.ts
@@ -1,22 +1,12 @@
 /* eslint-disable @typescript-eslint/no-type-alias */
-import {
-	type GuardedType,
-	isArray,
-	isBoolean,
-	IsInterface,
-	isString,
-	isUnion,
-	type TypeGuard,
-} from 'generic-type-guard';
-
-const isBooleanString: TypeGuard<'true' | 'false'> = (s: unknown): s is 'true' | 'false' =>
-	typeof s === 'string' && ['true', 'false'].includes(s);
+import { isArray, isBoolean, IsInterface, isSingletonStringUnion, isString, isUnion } from 'generic-type-guard';
+import type { GuardedType } from 'generic-type-guard';
 
 export const isNpmPackage = new IsInterface()
 	.withOptionalProperties({
 		name: isString,
 		workspaces: isArray(isString),
-		private: isUnion(isBoolean, isBooleanString),
+		private: isUnion(isBoolean, isSingletonStringUnion('true', 'false')),
 	})
 	.get();
 export type NpmPackage = GuardedType<typeof isNpmPackage>;
