fe4c4614a49c549bd8d7393c962e93edccbc7777
0xar-ds <sadden-rope-sleek@duck.com>
2025-09-20 07:33:00 -0300

    feat: add catalogs interface definitions
    see
    https://github.com/pnpm/pnpm/blob/main/workspace/read-manifest/src/index.ts
    
    feat: add named catalogs interface definitions

--- src/model/PnpmWorkspace.ts
+++ src/model/PnpmWorkspace.ts
@@ -1,6 +1,13 @@
 /* eslint-disable @typescript-eslint/no-type-alias */
 import { isArray, IsInterface, isString } from 'generic-type-guard';
+import { isArray, IsInterface, isOptional, isString } from 'generic-type-guard';
 import type { GuardedType } from 'generic-type-guard';
+
+export const isPnpmCatalog = new IsInterface().withStringIndexSignature(isString, true).get();
+export type PnpmCatalog = GuardedType<typeof isPnpmCatalog>;
+
+export const isPnpmNamedCatalogs = new IsInterface().withStringIndexSignature(isPnpmCatalog).get();
+export type PnpmNamedCatalogs = GuardedType<typeof isPnpmNamedCatalogs>;
 
 export const isPnpmWorkspace = new IsInterface()
 	.withProperties({
