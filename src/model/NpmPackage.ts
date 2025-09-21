4b43f029fb5be9833f49452af409b658eb39ece9
0xar-ds <sadden-rope-sleek@duck.com>
2025-09-21 08:36:25 -0300

    revert: "fix: workspaces configuration is optional to package manifests"
    This reverts commit 25316f39a8c44fcd5d24b6cf643b762c61425ae9.
    
    All properties are optional already, as the method used to model is
    <IsInterface>#withOptionalProperties

--- src/model/NpmPackage.ts
+++ src/model/NpmPackage.ts
@@ -1,19 +1,11 @@
 /* eslint-disable @typescript-eslint/no-type-alias */
-import {
-	isArray,
-	isBoolean,
-	IsInterface,
-	isOptional,
-	isSingletonStringUnion,
-	isString,
-	isUnion,
-} from 'generic-type-guard';
+import { isArray, isBoolean, IsInterface, isSingletonStringUnion, isString, isUnion } from 'generic-type-guard';
 import type { GuardedType } from 'generic-type-guard';
 
 export const isNpmPackage = new IsInterface()
 	.withOptionalProperties({
 		name: isString,
-		workspaces: isOptional(isArray(isString)),
+		workspaces: isArray(isString),
 		private: isUnion(isBoolean, isSingletonStringUnion('true', 'false')),
 	})
 	.get();
